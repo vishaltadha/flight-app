b92dad976a25ab44548f696d0901bda7a0935202
Vishal Tadha <macbookair@Vishals-MacBook-Air.local>
2021-11-28 16:54:53 +0530

    Bug Fixes - content height and input background

--- src/components/FilterBy/FilterBy.tsx
+++ src/components/FilterBy/FilterBy.tsx
@@ -38,60 +38,62 @@ export const FilterBy: React.FC<FilterByProps> = ({
     }
   };
   return (
-    <div>
-      <div className="card">
-        <h3>Price Range</h3>
-        <div className="row">
-          <div className="input-group">
-            <input
-              type="number"
-              id="minPrice"
-              className="pl-4"
-              onChange={(e: any) => {
-                setSelectedMinPrice(e.target.value);
-              }}
-              value={selectedMinPrice}
-            />
-            <label htmlFor="minPrice">Minimum Price</label>
-            <span className="input-prefix">$</span>
-          </div>
-          <div className="input-group">
-            <input
-              type="number"
-              id="maxPrice"
-              className="pl-4"
-              onChange={(e: any) => {
-                setSelectedMaxPrice(e.target.value);
-              }}
-              value={selectedMaxPrice}
-            />
-            <label htmlFor="maxPrice">Maximum Price</label>
-            <span className="input-prefix">$</span>
+    <div className="d-flex flex-column h-100">
+      <div className="flex-grow-1">
+        <div className="card">
+          <h3>Price Range</h3>
+          <div className="row">
+            <div className="input-group">
+              <input
+                type="number"
+                id="minPrice"
+                className="pl-4"
+                onChange={(e: any) => {
+                  setSelectedMinPrice(e.target.value);
+                }}
+                value={selectedMinPrice}
+              />
+              <label htmlFor="minPrice">Minimum Price</label>
+              <span className="input-prefix">$</span>
+            </div>
+            <div className="input-group">
+              <input
+                type="number"
+                id="maxPrice"
+                className="pl-4"
+                onChange={(e: any) => {
+                  setSelectedMaxPrice(e.target.value);
+                }}
+                value={selectedMaxPrice}
+              />
+              <label htmlFor="maxPrice">Maximum Price</label>
+              <span className="input-prefix">$</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="card">
-        <h3>Booking Class</h3>
-        <div className="m-0">
-          {FLIGHT_CLASSES.map((flightClass, idx) => {
-            return (
-              <div className="mt-2" key={idx}>
-                <input
-                  type="checkbox"
-                  name="bookingClass"
-                  id=""
-                  checked={
-                    !!selectedClassList.find(
-                      (selectedClass) => selectedClass === flightClass.id
-                    )
-                  }
-                  onChange={(e) => selectFlightClass(e, flightClass.id)}
-                  value={flightClass.id}
-                />
-                <span className="pl-3">{flightClass.label}</span>
-              </div>
-            );
-          })}
+        <div className="card">
+          <h3>Booking Class</h3>
+          <div className="m-0">
+            {FLIGHT_CLASSES.map((flightClass, idx) => {
+              return (
+                <div className="mt-2" key={idx}>
+                  <input
+                    type="checkbox"
+                    name="bookingClass"
+                    id=""
+                    checked={
+                      !!selectedClassList.find(
+                        (selectedClass) => selectedClass === flightClass.id
+                      )
+                    }
+                    onChange={(e) => selectFlightClass(e, flightClass.id)}
+                    value={flightClass.id}
+                  />
+                  <span className="pl-3">{flightClass.label}</span>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="row m-0 mt-2">
